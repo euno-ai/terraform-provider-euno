17417ca48f9be8984783c519b295107f2df2e234
EyalDlph <eyalf@getdelphi.io>
2025-10-03 19:35:19 -0700

    fix tests

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-framework/testing"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
@@ -13,7 +13,7 @@ import (
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"euno": providerserver.Serve(New("test")()),
+	"euno": providerserver.NewProtocol6WithError(New("test")()),
 }
 
 func testAccPreCheck(t *testing.T) {
@@ -22,16 +22,16 @@ func testAccPreCheck(t *testing.T) {
 }
 
 func TestAccEunoProvider(t *testing.T) {
-	testing.Test(t, testing.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []testing.TestStep{
+		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
 				Config: testAccFivetranIntegrationConfig(),
-				Check: testing.ComposeAggregateTestCheckFunc(
-					testing.TestCheckResourceAttr("euno_fivetran_integration.test", "name", "test-fivetran"),
-					testing.TestCheckResourceAttr("euno_fivetran_integration.test", "active", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("euno_fivetran_integration.test", "name", "test-fivetran"),
+					resource.TestCheckResourceAttr("euno_fivetran_integration.test", "active", "true"),
 				),
 			},
 		},
@@ -42,34 +42,28 @@ func testAccFivetranIntegrationConfig() string {
 	return `
 provider "euno" {
   account_id = 123
-  endpoint   = "http://localhost:8000"
+  server_url = "http://localhost:8000"
+  api_key    = "test-api-key"
 }
 
 resource "euno_fivetran_integration" "test" {
   name   = "test-fivetran"
   active = true
   
+  configuration {
+    api_key    = "test-key"
+    api_secret = "test-secret"
+    base_url   = "https://api.fivetran.com/v1"
+  }
+  
   schedule {
-    frequency       = "daily"
-    cron_expression = "0 6 * * * *"
+    time_zone   = "America/Los_Angeles"
+    repeat_on   = ["Mon", "Wed", "Fri"]
+    repeat_time = "10:00:00"
   }
   
   invalidation_strategy {
-    revision_id = null
-    ttl_days    = 7
-  }
-  
-  configuration {
-    auto_sync_enabled = true
-    connector         = "bigquery"
-    destination_schema_prefix = "test_"
-    connector_id     = "test-connector-id"
-    api_key          = "test-key"
-    api_secret       = "test-secret"
-    transform        = false
-    day_of_the_week  = 1
-    hour_of_the_day  = 6
-    version_id       = ""
+    ttl_days = 7
   }
 }
 `
